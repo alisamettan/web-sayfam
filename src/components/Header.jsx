4fc53cff3fe4d447cac974f886df6162d08e33df
alisamettan <alisamet_tan@hotmail.com>
2024-02-04 11:52:14 +0300

    local storage hooku eklendi ve darkmode için kullanıldı

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
           checked={darkMode}
           onChange={toggleDarkMode}
         />
-        <div className="w-11 h-6 bg-red-500 rounded-full peer peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-yellow after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-yellow-500 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
+        <div className="w-11 h-6 bg-red-500 rounded-full peer peer-focus:ring-4 peer-focus:ring-slate-300 dark:peer-focus:ring-slate-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-yellow after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-yellow-500 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-black peer-checked:bg-black"></div>
         <span className="ms-1.5  font-medium text-gray-500 dark:text-gray-300 border-r-2 border-gray-400 pr-5">
           {darkMode ? "Light Mode" : "Dark Mode"}
         </span>
--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export const DarkModeContext = createContext();
 
 export default function DarkModeContextProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
 
   function toggleDarkMode() {
     setDarkMode(!darkMode);
