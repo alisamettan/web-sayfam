376e0c9c704d768f148464f6be495dd87d44aca7
alisamettan <alisamet_tan@hotmail.com>
2024-02-04 18:31:33 +0300

    Language için context eklendi ve axios ile data api'ye postlanarak gerekli yerlerde kullanıldı

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { LanguageContext } from "../context/LanguageContext";
 
 export default function Header() {
   const { toggleDarkMode, darkMode } = useContext(DarkModeContext);
-  const { languageSwitcher } = useContext(LanguageContext);
+  const { languageSwitcher, currentLang } = useContext(LanguageContext);
   return (
     <div className="py-8 flex justify-end pr-10 bg-slate-100 dark:bg-[_#2A262B] ">
       <label className="relative inline-flex items-center cursor-pointer">
@@ -18,18 +18,36 @@ export default function Header() {
         />
         <div className="w-11 h-6 bg-red-500 rounded-full peer peer-focus:ring-4 peer-focus:ring-slate-300 dark:peer-focus:ring-slate-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-yellow after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-yellow-500 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-black peer-checked:bg-black"></div>
         <span className="ms-1.5  font-medium text-gray-500 dark:text-gray-300 border-r-2 border-gray-400 pr-5">
-          {darkMode ? "Light Mode" : "Dark Mode"}
+          {darkMode && currentLang === "en"
+            ? "Light Mode"
+            : darkMode && currentLang === "tr"
+            ? "Gündüz Modu"
+            : !darkMode && currentLang === "en"
+            ? "Dark Mode"
+            : "Gece Modu"}
         </span>
       </label>
-      <span className="ml-5 font-medium text-gray-500">
-        <a
-          onClick={languageSwitcher}
-          className="text-red-500 cursor-pointer hover:text-red-700"
-        >
-          TÜRKÇE'YE
-        </a>{" "}
-        GEÇ
-      </span>
+      {currentLang == "en" ? (
+        <span className="ml-5 font-medium text-gray-500">
+          <a
+            onClick={languageSwitcher}
+            className="text-red-500 cursor-pointer hover:text-red-700"
+          >
+            TÜRKÇE'YE
+          </a>{" "}
+          GEÇ
+        </span>
+      ) : (
+        <span className="ml-5 font-medium text-gray-500">
+          <a>SWITCH TO</a>{" "}
+          <a
+            onClick={languageSwitcher}
+            className="text-red-500 cursor-pointer hover:text-red-700"
+          >
+            ENGLISH
+          </a>
+        </span>
+      )}
     </div>
   );
 }
--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,28 +1,30 @@
 import { createContext, useEffect, useState } from "react";
 import { Data } from "../Data/Data";
 import axios from "axios";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export const LanguageContext = createContext();
 
 export default function LanguageContextProvider({ children }) {
   const initialLang = "en";
-  const [currentLang, setCurrentLang] = useState(initialLang);
-  const [currentData, setCurrentData] = useState(Data[currentLang]);
-
-  useEffect(() => {
-    axios
-      .post(`https://reqres.in/api/users?${currentLang}`, currentData)
-      .then((res) => {
-        console.log(currentData);
-        setCurrentData(res.data);
-
-        console.log(currentLang);
-      });
-  }, [currentLang]);
+  const [currentLang, setCurrentLang] = useLocalStorage("lang", initialLang);
+  //const [currentData, setCurrentData] = useState({ ...Data[currentLang] });
+  let currentData = Data[currentLang];
 
   function languageSwitcher() {
     setCurrentLang(currentLang == "en" ? "tr" : "en");
   }
+
+  useEffect(() => {
+    axios
+      .post(`https://reqres.in/api/users?${currentLang}`, Data[currentLang])
+      .then((res) => {
+        console.log(res.data);
+        currentData = res.data;
+
+        console.log(currentLang);
+      });
+  }, [currentLang]);
 
   return (
     <LanguageContext.Provider
