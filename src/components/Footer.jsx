9be20bc4a71dfd74c3350c93117211deea743aa5
alisamettan <alisamet_tan@hotmail.com>
2024-02-05 17:53:35 +0300

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,9 @@
-import { useContext, useEffect } from "react";
-import { Data } from "../Data/Data";
+import { useContext } from "react";
 import { LanguageContext } from "../context/LanguageContext";
-import axios from "axios";
 
 export default function Footer() {
-  const { currentData, setData, currentLang } = useContext(LanguageContext);
-  /*  useEffect(() => {
-    axios.get(`https://reqres.in/api/users?${currentLang}`).then((res) => {
-      setData(res.data);
-      console.log(res.data);
-    });
-  }, [currentLang, setData]); */
+  const { currentData } = useContext(LanguageContext);
+
   return (
     <div className="py-32 flex flex-col items-center text-center justify-center gap-20 dark:bg-[_#484148]  md:items-center  sm:flex-row">
       <p className="text-3xl">{currentData.footer.text}</p>
